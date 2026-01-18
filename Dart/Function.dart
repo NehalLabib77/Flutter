4dad3df568a38fb6d04437127fea48b8fcff3c23
NehalLabib77 <Nehal.pstu77@gmail.com>
2026-01-18 17:26:40 +0600

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness and dark mode support.
    - Implemented utility functions for creating a console and handling command line arguments in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including theme updates based on system preferences.
    - Added message handling for window events and DPI changes in win32_window.cpp and win32_window.h.

---
+++ Dart/Function.dart
@@ -0,0 +1,8 @@
+void main() {
+  findPerimeter(10, 5);
+}
+
+void findPerimeter(int length, int breadth) {
+  int perimeter = 2 * (length + breadth);
+  print("Perimeter of rectangle is $perimeter");
+}
